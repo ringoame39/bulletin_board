9468553912c479790c5c2e7fd1ed8046fe4be3eb
ringoame39 <megmin4520@gmail.com>
2016-11-18 16:19:37 +0900

    Change Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161117105420) do
+ActiveRecord::Schema.define(version: 20161117115001) do
+
+  create_table "chats", force: :cascade do |t|
+    t.text     "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "room_chats", force: :cascade do |t|
+    t.integer  "chat_id"
+    t.integer  "room_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "rooms", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +31,13 @@ ActiveRecord::Schema.define(version: 20161117105420) do
     t.string   "icon"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "user_chats", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "chat_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "user_rooms", force: :cascade do |t|
