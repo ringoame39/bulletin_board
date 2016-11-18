423c06afd342ca4291536967bf3481be161b8fe7
ringoame39 <megmin4520@gmail.com>
2016-11-18 20:39:56 +0900

    add processing Admin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def current_user
+    User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def admin?
+    if current_user
+      current_user.administrator?
+    else
+      false
+    end
+  end
 end
