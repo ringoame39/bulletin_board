1cccb3dbbf9066658c091864a1c4aefb39d705c3
ringoame39 <megmin4520@gmail.com>
2016-11-19 14:33:59 +0900

    add  allhome

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :rooms
   resources :users
   root :to => 'home#index'
-  get "/signin" => "users#sign_in"
+  get "/signin" => "users#sign_in", as: :sign_in
   post "/user_login" => "users#user_login"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
